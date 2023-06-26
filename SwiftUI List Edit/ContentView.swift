ebd4c0bb55a24c1598b83fedd4de9a0c9173d346
Theo Vora <theo.vora@gmail.com>
2023-06-26 16:27:08 -0400

    create list with 3 names and Edit button credit: hackingwithswift.com

---
+++ SwiftUI List Edit/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  SwiftUI List Edit
+//
+//  Created by Theo Vora on 6/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var users = ["Paul", "Taylor", "Adele"]
+
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(users, id: \.self) { user in
+                    Text(user)
+                }
+                .onDelete(perform: delete)
+            }
+            .toolbar {
+                EditButton()
+            }
+        }
+    }
+
+    func delete(at offsets: IndexSet) {
+        users.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI List Edit/SwiftUI_List_EditApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_List_EditApp.swift
+//  SwiftUI List Edit
+//
+//  Created by Theo Vora on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_List_EditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
